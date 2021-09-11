43942e421fd7b75ff10c7ee42bbf04e03d3c900d
roxanderve@gmail.com <https://gitlab.com/roxanderve/calendar.git>
2021-09-11 16:44:20 -0500

    commit inicial para sprint 1

---
+++ app/src/main/java/com/misiontic2022/app/tsuri/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.misiontic2022.app.tsuri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val iv: ImageView = findViewById(R.id.imageView)
+//        iv.setImageResource(R.drawable.japan)
+
+
+    }
+
+}
